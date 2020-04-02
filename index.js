037898a059b7f5ca18b34c94dd44276665caf75a
Eugene <eugenebondarenko76@gmail.com>
2020-04-03 02:35:04 +0300

    minor changes

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-const  io = require('socket.io')(8080);
+const io = require('socket.io')(8080);
+const { v4 } = require('uuid');
 
-let dialog = [];
+let users = [];
+let dialog = [{
+  name: `Server message`,
+  msg: `Connection has been established`,
+  timestamp: Date.now()
+}];
 
 io.on('connection', (socket) => {
   console.log('a user connected');
-  socket.broadcast.emit('hi', dialog);
+  socket.on('connect', () => {
+    socket.emit('dialog', dialog)
+  });
 
-  socket.on('chat message', (msg, name) => {
+  socket.emit('dialog', dialog);
+
+  socket.join('good-room', () => {
+    socket.to('good-room').emit('room-event', 'a new user connected')
+  });
+
+  socket.on('message', (name, msg) => {
     const message = {
       name,
       msg,
       timestamp: Date.now()
     };
-    socket.emit('message', message);
     dialog.push(message);
+    io.to('good-room').emit('dialog', dialog);
   });
 
-  io.on('disconnect', () => {
+  socket.on('disconnect', () => {
     console.log('a user disconnected');
   });
 });
 
+// setInterval(function() {
+//   socket.emit('dialog', dialog);
+// }, 4000);
 
+// socket.join('good-room', () => {
+//   socket.to('good-room').emit('room-event', 'a new user connected')
+// });
 
+// socket.broadcast.emit('dialog', dialog);
+
+// const NewUserObject = (userName, id, socket) => {
+//   return {
+//     user: userName,
+//     id: socket.id,
+//     socket: socket
+//   }
+// };
 
